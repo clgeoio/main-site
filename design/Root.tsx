35893e8368be4d9417c65b5d3747c1635228fe7e
Cody George <cody.george@airtasker.com>
2021-08-31 09:30:52 +1000

    desc

--- design/Root.tsx
+++ design/Root.tsx
@@ -9,7 +9,7 @@ const Root: React.FunctionComponent = ({ children }) => {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="clg software engineer" />
+        <meta name="description" content="clg - software engineer" />
         <title>clg</title>
       </Head>
       <Navbar title="clg" />
